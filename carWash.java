1c53b75aad7eacccca3607b6cd04c5392bc72497
DjStonie <valdonay@yahoo.dk>
2017-03-21 11:05:57 +0100

    Update carWash.java

--- carWash.java
+++ carWash.java
@@ -19,5 +19,42 @@ public class carWash {
     public static void login() {
 
     }
+    
+    
+    public static void ReadPrice()
+    {
+       try {
+       Scanner input = new Scanner(new File("Admin.txt"));
+       while (input.hasNextLine()) 
+           {
+               String linje = input.nextLine();
+               String[] linjesplit = linje.split(" ");
+               if (linjesplit[0].equals("economy:"))
+               {
+                  economy = Double.parseDouble(linjesplit[1]);
+                  return;
+               }
+               if (linjesplit[0].equals("standard:"))
+               {
+                  standard = Double.parseDouble(linjesplit[1]);
+                  return;
+               }
+               if (linjesplit[0].equals("deluxe:"))
+               {
+                  deluxe = Double.parseDouble(linjesplit[1]);
+                  return;
+               }
+               if (linjesplit[0].equals("rabat:"))
+               {
+                  Double rabatbeforecalc = Double.parseDouble(linjesplit[1]);
+                  rabat = (100-rabatbeforecalc)/100;
+                  return;
+               }
+           }
+       }
+       catch(FileNotFoundException e) {
+        e.printStackTrace();
+       }
+    }
 
 }
