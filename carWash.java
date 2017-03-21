d90fee8a5270f60f4671c87739807a70493061a0
jalte32 <jalte32@hotmail.com>
2017-03-21 11:32:10 +0100

    ChangePrices and ChangeOffer

--- carWash.java
+++ carWash.java
@@ -3,10 +3,10 @@ import java.io.*;
 
 public class carWash {
 
-    public double economy = 0;
-    public double standard = 0;
-    public double deluxe = 0;
-    public double rabat = 0;
+    public static double economy = 0;
+    public static double standard = 0;
+    public static double deluxe = 0;
+    public static double offer = 0;
 
     public static void main(String[] args) {
 
@@ -14,8 +14,40 @@ public class carWash {
 
     }
 
-    public static void login() {
+    public static void login() 
+    {
 
     }
-
+   
+   public static void ChangePrices()
+   {
+      Scanner sc = new Scanner (System.in);
+         
+      System.out.println("indtast nye priser på economy");
+      economy = sc.nextDouble();
+      
+      System.out.println("indtast nye priser på standard");
+      standard = sc.nextDouble();
+      
+      System.out.println("indtast nye priser på deluxe");
+      deluxe = sc.nextDouble();
+            
+      System.out.println("Ny pris på economy = " + economy);
+      System.out.println("Ny pris på standard = " + standard);
+      System.out.println("Ny pris på deluxe = " + deluxe);
+      
+     
+    }
+   public static void ChangeOffer()
+   {
+      Scanner sc = new Scanner (System.in);
+      
+      System.out.println("Indtast ny rabat");
+      offer = sc.nextDouble();
+      
+      System.out.println("Ny rabat = " + offer);
+      
+      
+   }
 }
+
