20f326bac5735672848a99873a0ec58d9153381c
DjStonie <valdonay@yahoo.dk>
2017-03-21 10:58:55 +0100

    Update carWash.java

--- carWash.java
+++ carWash.java
@@ -3,10 +3,12 @@ import java.io.*;
 
 public class carWash {
 
-    public double economy = 200;
-    public double standard = 400;
-    public double deluxe = 1000;
-    public double rabat = 0.8;
+    public static double economy = 200;
+    public static double standard = 400;
+    public static double deluxe = 1000;
+    public static double rabat = 0.8;
+    public static double saldo = 0;
+    public static String bruger = "";
 
     public static void main(String[] args) {
 
