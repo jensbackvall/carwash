e64ad579f63fe0eb58ccb3be4a14fc4263cb9091
Patrick Klæbel <vagabonden@outlook.com>
2017-03-21 11:34:33 +0100

    Update carWash.java

--- carWash.java
+++ carWash.java
@@ -14,7 +14,8 @@ public class carWash {
     public static double deluxe = 1000;
     public static double rabat = 0.8;
     public static double saldo = 0;
-    public static String bruger = "";
+    public static String brugernavn = "";
+    public static String password = "";
 >>>>>>> origin/master
 
     public static void main(String[] args) {
@@ -33,18 +34,18 @@ public class carWash {
    {
       Scanner sc = new Scanner (System.in);
          
-      System.out.println("indtast nye priser på economy");
+      System.out.println("indtast nye priser pÃ¥ economy");
       economy = sc.nextDouble();
       
-      System.out.println("indtast nye priser på standard");
+      System.out.println("indtast nye priser pÃ¥ standard");
       standard = sc.nextDouble();
       
-      System.out.println("indtast nye priser på deluxe");
+      System.out.println("indtast nye priser pÃ¥ deluxe");
       deluxe = sc.nextDouble();
             
-      System.out.println("Ny pris på economy = " + economy);
-      System.out.println("Ny pris på standard = " + standard);
-      System.out.println("Ny pris på deluxe = " + deluxe);
+      System.out.println("Ny pris pÃ¥ economy = " + economy);
+      System.out.println("Ny pris pÃ¥ standard = " + standard);
+      System.out.println("Ny pris pÃ¥ deluxe = " + deluxe);
       
      
     }
