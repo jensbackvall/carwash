fdca66602fe58fb98add53b35d388cf57a71ef15
Jens Bäckvall <jensbackvall@t-nova.org>
2017-03-21 10:18:04 +0100

    add variables

--- carWash.java
+++ carWash.java
@@ -3,6 +3,11 @@ import java.io.*;
 
 public class carWash {
 
+    public double economy = 0;
+    public double standard = 0;
+    public double deluxe = 0;
+    public double rabat = 0;
+
     public static void main(String[] args) {
 
         login();
