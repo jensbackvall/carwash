47682f3a9120a33af89e0a5256292b6c66a5006e
DjStonie <valdonay@yahoo.dk>
2017-03-21 11:41:49 +0100

    Update carWash.java

--- carWash.java
+++ carWash.java
@@ -9,7 +9,7 @@ public class carWash
     public static double deluxe = 1000;
     public static double rabat = 0.8;
     public static double saldo = 0;
-    public static String bruger = "";
+    public static String brugernavn = "";
 
     public static void main(String[] args) 
     {
