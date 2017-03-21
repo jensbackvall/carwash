5816c489e66eb665b6116760f571fb5c6a13444e
Jens Bäckvall <jensbackvall@t-nova.org>
2017-03-21 13:16:51 +0100

    fix indentations
    more indentations fixed for general readability

--- carWash.java
+++ carWash.java
@@ -23,7 +23,7 @@ public class carWash
 
     }
 
-   public static void buyWash(int user) {
+    public static void buyWash(int user) {
         try {
             Scanner console = new Scanner(System.in);
             Scanner userFile = new Scanner(new File(user + ".txt"));
@@ -68,36 +68,33 @@ public class carWash
         }
     }
 
-   public static void changePrices()
-   {
-      Scanner sc = new Scanner (System.in);
+    public static void changePrices()
+    {
+        Scanner sc = new Scanner (System.in);
 
-      System.out.println("indtast nye priser pÃ¥ economy");
-      economy = sc.nextDouble();
+        System.out.println("indtast nye priser pÃ¥ economy");
+        economy = sc.nextDouble();
 
-      System.out.println("indtast nye priser pÃ¥ standard");
-      standard = sc.nextDouble();
+        System.out.println("indtast nye priser pÃ¥ standard");
+        standard = sc.nextDouble();
 
-      System.out.println("indtast nye priser pÃ¥ deluxe");
-      deluxe = sc.nextDouble();
+        System.out.println("indtast nye priser pÃ¥ deluxe");
+        deluxe = sc.nextDouble();
 
-      System.out.println("Ny pris pÃ¥ economy = " + economy);
-      System.out.println("Ny pris pÃ¥ standard = " + standard);
-      System.out.println("Ny pris pÃ¥ deluxe = " + deluxe);
-
-
+        System.out.println("Ny pris pÃ¥ economy = " + economy);
+        System.out.println("Ny pris pÃ¥ standard = " + standard);
+        System.out.println("Ny pris pÃ¥ deluxe = " + deluxe);
     }
-   public static void changeRabat()
-   {
-      Scanner sc = new Scanner (System.in);
 
-      System.out.println("Indtast ny rabat");
-      rabat = (100. - sc.nextDouble())/100.;
+    public static void changeRabat()
+    {
+        Scanner sc = new Scanner (System.in);
 
-      System.out.println("Ny rabat = " + rabat);
+        System.out.println("Indtast ny rabat");
+        rabat = (100. - sc.nextDouble())/100.;
 
-
-   }
+        System.out.println("Ny rabat = " + rabat);
+    }
 
 
 
@@ -140,118 +137,119 @@ public class carWash
     }
 
     public static menu(String bruger)
-{
-    if( bruger.equals("01"))
-    admin();
-    else
-    customer();
-}
+    {
+        if( bruger.equals("01")) {
+            admin();
+        else
+        customer();
+        }
+    }
 
 
 
-public static void customer()
-{
+    public static void customer()
+    {
 
-   int choice;
-   boolean optionNotAvailable = true;
-   Scanner in = new Scanner(System.in);
+        int choice;
+        boolean optionNotAvailable = true;
+        Scanner in = new Scanner(System.in);
 
-   while (optionNotAvailable == true)
-   {
-      System.out.println("Indtast kategoriens nummer for at vaelge den");
-      System.out.println("1) Vaskemuligheder");
-      System.out.println("2) Optankning");
-      System.out.println("3) Konto oversigt");
-      System.out.println("4) Kvittering");
-      choice = in.nextInt();
+        while (optionNotAvailable == true)
+        {
+            System.out.println("Indtast kategoriens nummer for at vaelge den");
+            System.out.println("1) Vaskemuligheder");
+            System.out.println("2) Optankning");
+            System.out.println("3) Konto oversigt");
+            System.out.println("4) Kvittering");
+            choice = in.nextInt();
 
-      switch (choice)
-      {
-         case 1:  optionNotAvailable = false;
-                  admin();
-                  break;
+        switch (choice)
+        {
+            case 1: optionNotAvailable = false;
+                    admin();
+                    break;
 
-         case 2:  optionNotAvailable = false;
-                  System.out.print("Indsæt optanknings method");
-                  break;
+            case 2: optionNotAvailable = false;
+                    System.out.print("Indsæt optanknings method");
+                    break;
 
-         case 3:  optionNotAvailable = false;
-                  System.out.print("indsæt konto oversigt method");
-                  break;
+            case 3: optionNotAvailable = false;
+                    System.out.print("indsæt konto oversigt method");
+                    break;
 
-         case 4:  optionNotAvailable = false;
-                  System.out.print("indsæt kvittering method");
-                  break;
+            case 4: optionNotAvailable = false;
+                    System.out.print("indsæt kvittering method");
+                    break;
 
-         default: System.out.println("Din indtastning er invalid, prøv igen");
-      }
-   }
-}
+            default: System.out.println("Din indtastning er invalid, prøv igen");
+        }
+        }
+    }
 
-public static void admin()
-{
-   boolean optionNotAvailable = true;
-   int choice;
-   Scanner in = new Scanner(System.in);
+    public static void admin()
+    {
+        boolean optionNotAvailable = true;
+        int choice;
+        Scanner in = new Scanner(System.in);
 
-   while( optionNotAvailable == true )
-   {
-      System.out.println("Indtast kategoriens nummer for at vaelge den");
-      System.out.println("1) aendre priser");
-      System.out.println("2) Se statestik");
-      choice = in.nextInt();
+        while( optionNotAvailable == true )
+        {
+            System.out.println("Indtast kategoriens nummer for at vaelge den");
+            System.out.println("1) aendre priser");
+            System.out.println("2) Se statestik");
+            choice = in.nextInt();
 
-      switch(choice)
-      {
-         case 1:  optionNotAvailable = false;
-                  System.out.println("Indsæt aendre priser method");
-                  break;
+        switch(choice)
+        {
+            case 1: optionNotAvailable = false;
+                    System.out.println("Indsæt aendre priser method");
+                    break;
 
-         case 2:  optionNotAvailable = false;
-                  System.out.println("Indsæt se statistik method");
-                  break;
+            case 2: optionNotAvailable = false;
+                    System.out.println("Indsæt se statistik method");
+                    break;
 
-         default: System.out.println("Din indtastning er invalid, prøv igen");
-                  break;
-      }
-   }
-}
+            default: System.out.println("Din indtastning er invalid, prøv igen");
+            break;
+        }
+    }
+    }
 
-  public static void refill()
-  {
-      System.out.println("Optankning");
-      System.out.println("Indtast beløb (Mellem 1-10.000)");
-      Scanner console = new Scanner(System.in);
-      double amount = console.nextDouble();
-      System.out.println("***Wash card ejets***");
-      System.out.println();
-      System.out.println("Indsæt kreditkort");
+    public static void refill()
+    {
+        System.out.println("Optankning");
+        System.out.println("Indtast beløb (Mellem 1-10.000)");
+        Scanner console = new Scanner(System.in);
+        double amount = console.nextDouble();
+        System.out.println("***Wash card ejets***");
+        System.out.println();
+        System.out.println("Indsæt kreditkort");
 
-      System.out.println("***Press enter when credit card has been inserted***");
-      console.nextLine();
-      console.nextLine();
-      System.out.println("Indtast pinkode");
-      int pin = 1234;
-      int enteredPin = console.nextInt();
-      int count = 0;
-      while(enteredPin != pin && count < 2) {
-      System.out.println("Forkert pinkode - Prøv igen");
-      enteredPin = console.nextInt();
-      count++;
-      }
-      if(enteredPin==pin){
-      System.out.println(amount + "kr");
-      System.out.println("Tryk 1 og enter for at godkende");
-      System.out.println("Tryk 2 og enter for at fortryde");
-      }
-      else{
-      System.out.print("Korter er spærret");
-      }
-      int godkendFortryd = console.nextInt();
-      while(godkendFotryd != 1 || godkendFotryd != 2) {
+        System.out.println("***Press enter when credit card has been inserted***");
+        console.nextLine();
+        console.nextLine();
+        System.out.println("Indtast pinkode");
+        int pin = 1234;
+        int enteredPin = console.nextInt();
+        int count = 0;
+        while(enteredPin != pin && count < 2) {
+            System.out.println("Forkert pinkode - Prøv igen");
+            enteredPin = console.nextInt();
+            count++;
+        }
+        if(enteredPin==pin){
+            System.out.println(amount + "kr");
+            System.out.println("Tryk 1 og enter for at godkende");
+            System.out.println("Tryk 2 og enter for at fortryde");
+        }
+        else{
+            System.out.print("Korter er spærret");
+        }
+        int godkendFortryd = console.nextInt();
+        while(godkendFotryd != 1 || godkendFotryd != 2) {
 
-      }
-  }
+        }
+    }
 
 
 }
